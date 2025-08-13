2aa84652cd888a1019a5efc43c96a2cd500fa5b8
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-13 18:07:36 -0300

    Se cumpleto el ultimo tp de Hello world (parte 3)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Tp1_Helloworld from './components/Tp1_helloworld'
 import Tp2_Helloworld from './components/Tp2_Helloworld'
+import Tp3_helloworld from './components/Tp3_helloworld'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <main className='container text-center my-5'>
       <Tp1_Helloworld />
       <Tp2_Helloworld saludo="My friend" />
+      <Tp3_helloworld saludo="My friend" />
       
     </main>
   )
