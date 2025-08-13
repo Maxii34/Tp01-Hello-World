ca1a3adf83fe4e07924fc2e4c2c28d56cdd07c6b
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-13 15:21:58 -0300

    Se agrego el componente del tp02

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
-import Helloworld from './components/Helloworld'
+import Tp1_Helloworld from './components/Tp1_helloworld'
+import Tp2_Helloworld from './components/Tp2_Helloworld'
 
 function App() {
 
   return (
     <main className='container text-center my-5'>
-      <Helloworld />
+      <Tp1_Helloworld />
+      <Tp2_Helloworld saludo="My friend" />
+      
     </main>
   )
 }
---
+++ src/components/Tp2_helloworld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tp2_Helloworld = ({saludo}) => {
+    return (
+        <div className='my-5 border-bottom'>
+            <h1 className='fs-2'>Hello World {saludo} </h1>
+            <p className='text-muted'>Modificar el componente anterior y enviar mediante props el valor “My friend” de manera que el mensaje quede de la siguiente forma:</p>
+        </div>
+    );
+};
+
+export default Tp2_Helloworld;
