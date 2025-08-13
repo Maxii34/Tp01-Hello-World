118702b97820bcc4cd0445b38203d4268c86afa1
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-13 14:28:41 -0300

    Se agrego el primer tp01 de helloworld

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Helloworld from './components/Helloworld'
 
 function App() {
 
   return (
-    <>
- 
-    </>
+    <main className='container text-center my-5'>
+      <Helloworld />
+    </main>
   )
 }
 
