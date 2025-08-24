f029ca5ed2218731365c2d4378c3d7a3b188abbd
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-23 22:03:48 -0300

    Se agrego nav, footer y se le aplico css para separar el nav del footer.

--- src/components/Tp2_helloworld.jsx
+++ src/components/Tp2_helloworld.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Tp2_Helloworld = ({saludo}) => {
     return (
-        <div className="my-5 card p-2 shadow">
+        <div className="my-5 card p-2 shadow w-auto">
             <h1 className='fs-2'>Hello World {saludo} </h1>
             <p className='text-muted'>02- Modificar el componente anterior y enviar mediante props el valor “My friend” de manera que el mensaje quede de la siguiente forma:</p>
         </div>
--- src/components/Tp3_helloworld.jsx
+++ src/components/Tp3_helloworld.jsx
@@ -1,20 +1,20 @@
-import React from "react";
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 const Tp3_helloworld = ({ saludo }) => {
   const [msj] = useState("(from changed state)");
   const [ocultar, setOcultar] = useState(false);
 
   return (
-    <div className="my-5 card p-2 shadow">
+    <div className="my-5 card p-2 shadow w-auto">
       <h1 className="fs-2">
         Hello {saludo} {ocultar && <span>{msj}</span>}{" "}
       </h1>
       <p className="text-muted">03- Modificar el programa anterior agregando al componente un state con la clave msj: “(from changed state)”, además agregar un botón al presionar este botón (onClick), el state de la clave msj se debe mostrar por pantalla.</p>
       <div>
-        <button className="btn btn-dark shadow" onClick={() => setOcultar(!ocultar)}>
+        <Button className="shadow" variant="dark" onClick={() => setOcultar(!ocultar)}>
           Click me
-        </button>
+        </Button>
       </div>
     </div>
   );
